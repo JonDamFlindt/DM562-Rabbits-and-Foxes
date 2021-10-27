0e4a63e76c2e643cd0c8778dba9fa4ca6746ceb9
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2021-10-27 18:57:28 +0200

    Update reporting.py
    Added dummy functions to simulation.py

--- reporting.py
+++ reporting.py
@@ -1,1 +1,14 @@
+def print_summary(results):
+  pass
 
+def plot_pop_size(results):
+  pass
+
+def plot_lifespan(results):
+  pass
+
+def plot_energy(results):
+  pass
+
+def plot_kills(results):
+  pass
