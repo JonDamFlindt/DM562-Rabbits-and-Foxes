425ee1689e74ef6154bed2a82e080ce7cfd1b285
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2021-11-02 10:48:16 +0100

    Update foxes_and_rabbits.py
    Added minor code as a state machine-type menu for inputs, needs slight rework

--- foxes_and_rabbits.py
+++ foxes_and_rabbits.py
@@ -1,3 +1,26 @@
-import parameters
+import parameters as param
 import simulation as sim
 import reporting as report
+
+setup_type = None
+
+while True:
+  while setup_type not in ['quick', 'advanced', 'cancel', 'exit']: # while-loop to ensure valid setup choice
+    print(f"Quick parameter setup:\n{}\n{}\n{}")
+    print("")
+    setup_type = input("Please choose whether you want to use quick setup or advanced setup, or exit the program:\n").lower()
+    if setup_type not in ['quick', 'advanced', 'cancel', 'exit']:
+      input("Unrecognized command, please try again (hit enter).")
+    
+   
+  if setup_type in ['cancel', 'exit']:
+    print(f"Terminating the program.")
+    break
+  
+  if setup_type == 'quick':
+    pass
+  
+  if setup_type == 'advanced':
+    pass
+  
+
