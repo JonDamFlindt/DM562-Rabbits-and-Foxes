c300735808b28e71fe1b4b2b74e18b9192f7510b
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2021-11-06 05:41:48 +0100

    Update reporting.py
    Added given mock functions

--- reporting.py
+++ reporting.py
@@ -1,14 +1,45 @@
+"""
+This module provides functions for analysing and reporting results of a simulation.
+
+WARNING: this is a mock implementation of the module.
+"""
+
 def print_summary(results):
-  pass
+  """
+  Prints a summary of the simulation results and basic statistics.
+  """
+  _print_info(print_summary)
 
 def plot_pop_size(results):
-  pass
+  """
+  Plots population sizes against time. 
+  """
+  _print_info(plot_pop_size)
 
 def plot_lifespan(results):
-  pass
+  """
+  Plots lifespans across population idividuals. 
+  """
+  _print_info(plot_lifespan)
 
 def plot_energy(results):
-  pass
+  """
+  Plots the total energry over the life of eah individual. 
+  """
+  _print_info(plot_energy)
 
 def plot_kills(results):
-  pass
+  """
+  Displays the distribution of kills.
+  """
+  _print_info(plot_kills)
+
+
+def plot_all(results):
+  """
+  Displays the distribution of kills.
+  """
+  _print_info(plot_kills)  
+  
+def _print_info(f):
+  print(f"INFO: called function '{f.__name__}' of module 'reporting'. This is a mock implementation.")
