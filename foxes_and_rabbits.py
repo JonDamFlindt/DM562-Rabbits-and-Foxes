9f475bd1edb8cfe48ea2ca0f4f6a9e8d73323c00
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2021-11-10 17:45:04 +0100

    Update foxes_and_rabbits.py
    Removed unneeded items.

--- foxes_and_rabbits.py
+++ foxes_and_rabbits.py
@@ -1,14 +1,12 @@
 import parameters as param
 import simulation as sim
 import reporting as report
-import typing
-import json
-# Setting up menus
-start_menu = ['Display parameters', 'Quick setup', 'Advanced setup', 'Run', 'Quit']
-advanced_menu = ['World', 'Rabbit population', 'Fox population', 'Execution', 'Done']
-reporting_menu = ['Print summary', 'Plot pop. size / time', 'Plot lifespan', 'Plot energy', 'Plot kills distrubution', 'Plot all', 'Quit']
+from typing import List
 
-config_file = 'parameters.json'
+# Setting up menus
+start_menu = ['display parameters', 'quick setup', 'advanced setup', 'run', 'quit']
+advanced_menu = ['world', 'rabbits', 'foxes', 'execution', 'done']
+reporting_menu = ['print summary', 'plot pop. size / time', 'plot lifespan', 'plot energy', 'plot kills distrubution', 'quit']
 
 quick_params = """QUICK PARAMETERS:
 North/South length (height, positive int): {}
@@ -18,63 +16,60 @@ West/East length (width, positive int): {}
 Visuals (True/False): {}
 """
 
-def menu(menu_list: typing.List[str]) -> str:
-    """Menu function
-    Prints a list of options which the user can choose as input given a list.
-    Precondition: menu_list is a list of strings.
-    """
-    state = None # Initialize variable
+"""ADVANCED PARAMETERS:
+{}
+{}
+{}
+{}
+{}
+{}
+"""
+
+def menu(menu_list: List[str]) -> str:
+  """Menu function
+  Prints a list of options which the user can choose as input given a list.
+  """
+  state = None # Initialize variable
+  
+  viable_states = range(len(menu_list)) # Get viable states from the menu
+     
+  while state not in viable_states:
+    print("Action selection:")
+    for option in viable_states: # Prints all menu options
+      print(str(option + 1) + '.', menu_list[option].capitalize())
     
-    viable_states = range(len(menu_list)) # Get viable states from the menu
-       
-    while state not in viable_states:
-        print("Action selection:")
-        for option in viable_states: # Prints all menu options
-            print(str(option + 1) + '.', menu_list[option])
+    state = input("Awaiting input: ").lower().replace('.', '').split(' ') # Handles any extra spaces in input
+    state = ' '.join([word for word in state if word != ''])
+    print()
+
+    
+    try: # Reasoning for try/except here is that even something like 0.2 is not a viable state, and we allow "1." as a valid input.
+      state = int(state) - 1 # Raises ValueError if state is not int
+      if state in viable_states:
+        return menu_list[state]
+    
+    except ValueError: # Checks if state is string in menu
+      if state in menu_list:
+        return state
         
-        state = input("Awaiting input: ").replace('.', '').split(' ')
-        state = ' '.join([word for word in state if word != '']).capitalize()
-        # Above handles any extra spaces the user may insert into the input.
-        
-        print()
-        try:
-            state = int(state) - 1 # Raises ValueError if state is not int
-            if state in viable_states:
-                return menu_list[state]
-        except ValueError: # Checks if state is string in menu
-            if state in menu_list:
-                return state
-            state = None
-        
-        input("Invalid input, please try again (hit enter).\n") # Only printed if invalid state
+  input("Invalid input, please try again (hit enter).\n") # Only printed if invalid state
 
 state = None # Default state
 
-while state != start_menu[-1]: # As long as not "Quit"
+while state != start_menu[-1]: # As long as not "quit"
   if state not in start_menu:
-    with open(config_file) as file:
-      pass
     print(quick_params)
   state = menu(start_menu)
 
   if state == start_menu[0]: #Display parameters
-    with open(config_file) as file:
-      parameters = json.load(file)
-    for category in parameters:
-      print(f'[{category}]')
-      for setting in parameters[category]:
-        print(f'{setting}: {parameters[category][setting]}')
-      print()
+    print()
 
 
   if state == start_menu[1]: #Quick setup
-    with open(config_file) as parameters:
-      pass
+    pass      
 
   if state == start_menu[2]: #Advanced setup
     while state != advanced_menu[-1]: # As long as not "Done/go back"
-      with open(config_file) as parameters:
-        pass
 
       state = menu(advanced_menu)
       if state == advanced_menu[0]: #World
@@ -87,9 +82,7 @@ while state != start_menu[-1]: # As long as not "Quit"
         pass
 
   if state == start_menu[3]: #Run
-    with open(config_file) as parameters:
-      pass
-        
+    sim_data = sim.Run()
     while state != reporting_menu[-1]: # As long as not "Quit"
       state = menu(reporting_menu)
       
