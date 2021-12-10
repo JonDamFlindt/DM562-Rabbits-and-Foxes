22ab716160f03a95078368166b08e87e231c12dc
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2021-12-10 18:47:53 +0100

    Update simulation.py
    Bug fixed

--- simulation.py
+++ simulation.py
@@ -10,7 +10,14 @@ from typing import Union, List, Tuple
 
 def run(parameters):
     patches = []
+    sim_step = 0
+    all_dead = False
     stats = results.SimulationStats()
+
+    for x in range(parameters.world.west_east_length):
+        stats.kills_per_patch.append([])
+        for y in range(parameters.world.north_south_length):
+            stats.kills_per_patch[x].append(0)
     
     for i in range(parameters.world.area()):
         patches.append(entities.Patch(i % parameters.world.west_east_length, i // parameters.world.west_east_length))
@@ -23,7 +30,7 @@ def run(parameters):
             stats.rabbits.total += 1
         
 
-    def init_animals(animal: entities.Animal, pop_parameters: Union[parameters.rabbits, parameters,foxes]):
+    def init_animals(animal_entity: type(entities.Animal), pop_parameters: type(parameters.rabbits)):
         """"Adds the initial population to random patches of grass in the world."""
         counter = 0
         while counter < pop_parameters.initial_size:
@@ -31,8 +38,8 @@ def run(parameters):
             
             if len(patches[index].animals()) == 0 or len(patches[index].animals()) == 1 and type(patches[index].animals()[0]) is not animal_entity:
                 # If the patch is empty, or if there is another animal but it isn't of the same type, add the animal.
-                patches[index].add(animal(pop_parameters, patches[index], random.randint(0, pop_parameters.max_age)))
-                data_on_birth(animal)
+                patches[index].add(animal_entity(pop_parameters, patches[index], random.randint(0, pop_parameters.max_age)))
+                data_on_birth(animal_entity)
                 counter += 1
 
 
@@ -46,55 +53,59 @@ def run(parameters):
         popStats.age_at_death.append(animal.age)
 
         ### CAUSE OF DEATH
-        if animal.energy <= 0: # Not enough food; mainly an issue for wolves.
+        if animal.energy() <= 0: # Not enough food; mainly an issue for wolves.
                 popStats.dead_by_starvation += 1
-        elif animal.was_killed(): # Only occurs for rabbits
-            stats.dead_by_predation += 1
+        elif type(animal) == entities.Rabbit and animal.was_killed(): # Only occurs for rabbits
+            popStats.dead_by_predation += 1
             coords = animal.patch().coordinates()
-            stats.kills_per_patch[coords[0][coords[1]]] += 1
+            stats.kills_per_patch[coords[0]][coords[1]] += 1
             
         else: # Since we cannot get the species parameters from the animal and this is the last possible scenario, else.
             popStats.dead_by_old_age += 1
         
 
 
-    def get_legal_move(current_patch: entities.Patch, moving_animal: entities.Animal, is_reproducing: bool = False) -> Union[entities.Patch, None]:
+    def get_legal_move(current_patch: type(entities.Patch), moving_animal: type(entities.Animal), is_reproducing: bool = False) -> Union[entities.Patch, None]:
         """Checks if surrounding tiles are valid for movement and/or reproduction."""
+        all_moves = []
         legal_moves = []
         coords = list(current_patch.coordinates())
         
-        legal_moves.append([coords[0], coords[1] + 1]) # Up
-        legal_moves.append([coords[0], coords[1] - 1]) # Down
-        legal_moves.append([coords[0] - 1, coords[1]]) # Left
-        legal_moves.append([coords[0] + 1, coords[1]]) # Right
+        all_moves.append([coords[0], coords[1] - 1]) # Up
+        all_moves.append([coords[0], coords[1] + 1]) # Down
+        all_moves.append([coords[0] - 1, coords[1]]) # Left
+        all_moves.append([coords[0] + 1, coords[1]]) # Right
 
 
         if parameters.world.is_toroid: # If toroid, make coordinates "loop".
-            for i in range(legal_moves): #Re-index all points to the visible plane/within range
-                legal_moves[i][0]  %= parameters.world.west_east_length
-                legal_moves[i][1] //= parameters.world.west_east_length
+            for i in range(len(all_moves)): #Re-index all points to the visible plane/within range
+                all_moves[i][0]  %= parameters.world.west_east_length
+                all_moves[i][1] %= parameters.world.north_south_length
                     
         else: # If not toroid
-            for i in range(legal_moves):
-                if legal_moves[i][0] < 0 or coords[1] < 0 or coords[0] > parameters.world.west_east_length - 1 or coords[0] > parameters.world.north_south_length - 1:
-                    legal_moves.pop(i) # Remove all illegal moves, i.e. points whose coordinates are out of range.
+            for i in range(len(all_moves)):
+                if all_moves[i][0] < 0 or all_moves[i][1] < 0 or all_moves[i][0] >= parameters.world.west_east_length or all_moves[i][0] >= parameters.world.north_south_length:
+                    all_moves.pop(i) # Remove all illegal moves, i.e. points whose coordinates are out of range.
 
 
         index_from_coords = lambda x,y: x + parameters.world.west_east_length * y #Gets index from x and y value
 
-        for new_coords in legal_moves:
-            potential_patch = patches[index_from_coords(*new_coords)] 
-            patch_animals = potential_patch.animals()
-            if len(patch_animals) == 2 or len(patch_animals) == 1 and moving_animal.same_species_in(potential_patch):
-                legal_moves.pop(coords) # Remove all full patches
-            if len(patch_animals) == 1 and moving_animal.predators_in(potential_patch) and is_reproducing:
-                legal_moves.pop(coords) # Remove all predator patches if reproducing
+        for i in all_moves:
+            legal_moves.append(i)
+            
+        for coordinate_index in range(len(all_moves)):
+            potential_patch = patches[index_from_coords(*all_moves[coordinate_index])]
+            
+            if moving_animal.same_species_in(potential_patch):
+                legal_moves.remove(all_moves[coordinate_index]) # Remove all patches with same species in it
+            elif moving_animal.predators_in(potential_patch) and is_reproducing:
+                legal_moves.remove(all_moves[coordinate_index]) # Remove all predator patches if reproducing
 
         if legal_moves == []:
             move = None
         else:
-            move = patches(index_from_coords(*random.choice(legal_moves)))
-
+            move = patches[index_from_coords(*random.choice(legal_moves))]
+        
         return move
 
 
@@ -102,34 +113,41 @@ def run(parameters):
     init_animals(entities.Fox, parameters.foxes)
 
     world = vis.ColourGraphics(parameters.execution.max_steps, patches, parameters.world.west_east_length, parameters.world.north_south_length)
-    world.start()    
-    for i in range(parameters.execution.max_steps):
+    world.start()
+
+    while sim_step < parameters.execution.max_steps and not all_dead:
+        all_dead = True
+        
         for patch in patches:
             patch.tick()
-            
             if len(patch.animals()) > 0:
-                for animal_index in range(len(patch.animals())):
-                    animal = patch.animals()[animal_index]
-                    animal.tick()
+                for animal in patch.animals():
+                    if animal.is_alive():
+                        all_dead = False
+                        animal.tick()
 
-                    if len(animal.patch().animals()) == 2 and type(animal.patch().animals()[0]) is entities.Rabbit: 
-                        animal.kill() #Kill bunny if starting turn on fox square
-                    else:
-                        animal.feed() #Otherwise eat
+                        if animal.predators_in(animal.patch()):
+                            animal.kill() #Kill bunny if starting turn on fox square
+                        else:
+                            animal.feed() #Otherwise eat
 
-                    if not animal.is_alive(): #If dead
-                        data_on_death(animal)
-                        animal.patch().remove(animal)
-                        
-                    if animal.can_reproduce():
-                        baby_patch = get_legal_move(animal.patch())
-                        if baby_patch is not None:
-                            animal.reproduce(baby_patch)
-                            data_on_birth(animal) # Note that we can pass animal as we only need its type
-                    
-                            
+                        if not animal.is_alive(): #If dead
+                            data_on_death(animal)
     
+                        
+                        if animal.can_reproduce():
+                            baby_patch = get_legal_move(animal.patch(), animal, True)
+                            if baby_patch is not None:
+                                animal.reproduce(baby_patch)
+                                data_on_birth(animal) # Note that we can pass animal as we only need its type
+                                
+                        elif animal.is_alive():
+                            new_patch = get_legal_move(animal.patch(), animal)
+                            if new_patch is not None:
+                                animal.move_to(new_patch)
 
+        sim_step += 1
         world.update(i)
-
+        
     world.close()
+    return stats
