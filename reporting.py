4aa7d39f00860cc5c47569b235112a0a3ba3d625
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2022-01-09 21:43:57 +0100

    Update reporting.py

--- reporting.py
+++ reporting.py
@@ -5,7 +5,10 @@ from results import *
 
 def print_summary(results: SimulationStats):
    """Prints a short summary regarding the populations of the simulation."""
-   def print_info(animal, data):
+
+   for item in [['FOXES', results.foxes],['RABBITS', results.rabbits]]:
+      animal = item[0]
+      data = item[1]
       print(f"""
 [{animal}]
 Population
@@ -21,15 +24,15 @@ starvation: {data.dead_by_starvation}
 predation: {data.dead_by_predation}
 """)
 
-   print_info('FOXES', results.foxes)
-   print_info('RABBITS', results.rabbits)
 
-   
-
+# Since we have to plot each population and their total,
+# we will use for-loops with preset colors and styles
 plt_style = ['--C1', '--C7', '-k']
 plt_legend = ['Foxes','Rabbits','Total']
 
-def _setup_plot(data, plot_title, x_label, y_label):
+
+def _setup_plot(data, plot_title: str, x_label: str, y_label: str):
+   """Generic function to set up the plot."""
    plt.title(plot_title)
    plt.xlabel(x_label)
    plt.ylabel(y_label)
@@ -42,6 +45,7 @@ def _setup_plot(data, plot_title, x_label, y_label):
 
 
 def plot_pop_size(results: SimulationStats):
+   """Plots the size of the population for each step."""
    total_size_per_step = [results.foxes.size_per_step[i] + results.rabbits.size_per_step[i] for i in range(len(results.foxes.size_per_step))]
    plt_data = [results.foxes.size_per_step,
                results.rabbits.size_per_step,
@@ -51,35 +55,27 @@ def plot_pop_size(results: SimulationStats):
    
 
 def plot_lifespan(results: SimulationStats):
+   """Plots the lifespan of every individual that has ever lived during the simulation."""
    plt_data = [results.foxes.age_at_death, results.rabbits.age_at_death]
-   fig, axes = plt.subplots(2)
-   for i in range(len(axes)):
-      plt_data[i].sort()
-      # Since this is done at death and not at step,
-      # this looks erratic if we do not sort this beforehand
-      axes[i].plot(plt_data[i],
-                   plt_style[i],
-                   linewidth=1,
-                   scalex=True)
-      axes[i].set_title(plt_legend[i])
-   fig.suptitle("Lifespans")
+   _setup_plot(plt_data, 'Lifespans', 'Individuals', 'Age')
    plt.show()
 
-   
+
 def plot_energy(results: SimulationStats):
+   """Plots the average energy of the animals per step."""
    plt_data = [results.foxes.avg_energy_per_step,
                results.rabbits.avg_energy_per_step,
                results.avg_energy_per_step]
    _setup_plot(plt_data, 'Average energy over time', 'Steps', 'Energy')
    plt.show()
 
-   
+
 def plot_kills(results: SimulationStats):
    """Plots kills on the grid."""
-   all_kills = [kill for patch in results.kills_per_patch for kill in patch]
+   max_kills = max([kill for patch in results.kills_per_patch for kill in patch])
    colormap = matplot.colors.LinearSegmentedColormap.from_list('kills',['white','yellow','orange','red'], 256)
    plt.imshow(results.kills_per_patch, cmap=colormap)
    plt.title('Distribution of kills by predators.')
-   plt.colorbar(ticks=range(max(all_kills)))
+   plt.colorbar(ticks=range(max_kills+1))
    plt.show()
-  
+   
