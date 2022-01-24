1193164476267407e8a9347525a84c595574c0cf
Jon Dam Flindt <79864862+JonDamFlindt@users.noreply.github.com>
2022-01-24 17:49:08 +0100

    Update foxes_and_rabbits.py

--- foxes_and_rabbits.py
+++ foxes_and_rabbits.py
@@ -1,19 +1,13 @@
 import parameters
 import simulation as sim
 import reporting as report
-from typing import List
+from typing import List, Union, Tuple
+import sys
 
 # Setting up menus
 start_menu = ['display parameters', 'quick setup', 'advanced setup', 'run', 'quit']
 advanced_menu = ['world', 'rabbits', 'foxes', 'execution', 'done']
 reporting_menu = ['print summary', 'plot pop. size / time', 'plot lifespan', 'plot energy', 'plot kills distrubution', 'quit']
-
-# Setting up variable changers
-quick_vars = ['north/south length (positive int)', 'west/east length (positive int)', 'initial rabbits (int, less than world area)', 'initial foxes (int, less than world area)', 'max steps (int)', 'batch mode (bool)']
-adv_world = ['toroidal (bool)', 'north/south length (positive int)', 'west/east length (positive int)']
-adv_pop = ['initial size (int, less than world area)', 'max age (int)', 'max energy (int)', 'metabolism (int)', 'reproduction probability (float between 0 and 1)', 'min reproduction age (int)', 'min reproduction energy (int)']
-adv_exe = ['max steps (int)', 'batch mode (bool)']
-
 
 quick_param_msg = """QUICK PARAMETERS:
 world: {} by {} (n/s by w/e)
@@ -22,9 +16,7 @@ initial foxes: {}
 max steps: {}
 mode: {}"""
 
-
 params = parameters.Simulation()
-
 
 
 def _user_input(msg: str = 'Awaiting input'):
@@ -47,7 +39,7 @@ def menu(menu_list: List[str]) -> str:
   viable_states = range(len(menu_list)) #Get viable states from the menu
 
   while state not in menu_list:
-    print("\nAction selection:")
+    print("\nSelection:")
     for option in viable_states: #Prints all menu options
       print(str(option + 1) + '.', menu_list[option].capitalize())
 
@@ -66,140 +58,110 @@ def menu(menu_list: List[str]) -> str:
   return state
 
 
-def input_parameters(current_params: list, msg_list: List[str]) -> list:
-  """Returns a list of valid parameters, given a list of current parameters and input messages."""
+def bool_input(true_option: str, false_option: str, msg: str) -> bool:
+  """Input method for parameters of bool values. First bool should be the True statement, second one should be False."""
+  print(f'{msg}\n')
+  user = menu([true_option, false_option])
+  return user == true_option
+  
 
-  assert len(current_params) == len(msg_list), "List input length mismatch"
-  user_parameters = [None] * len(current_params)
-  print("Parameter setup, skip parameter change by pressing enter or w/ '~' or '-')")
-
-  for i in range(len(msg_list)): # Go through every parameter
-    current_type = type(current_params[i])
-    while type(user_parameters[i]) is not current_type: # Wait until either "skip" or correct input type
-      user_parameters[i] = _user_input(msg_list[i] + f' (currently {current_params[i]})') # Print setting
-
-      # Below are checks to see if inputs are valid types
-      type_bool = current_type is bool and user_parameters[i] in ['true', 'false']
-      type_int = current_type is int and is_integer(user_parameters[i])
-      type_float = current_type is float and user_parameters[i].replace('.', '', 1).isdecimal()
-
-      if user_parameters[i] in ['', '~', '-']:
-        user_parameters[i] = current_params[i]
-        print("Skipping...")
-
-      elif '-' in user_parameters[i]: # Stops negative inputs
-        print("Hyphens and minus signs (i.e. negative numbers) are not allowed, try again.")
-
-      # If types are valid, evaluate (this converts the string to its correct type, and only if it is of that type)
-      elif type_bool or type_int or type_float:
-        user_parameters[i] = eval(user_parameters[i].capitalize()) # Capitalize in case of bool
-        if type_float:
-          user_parameters[i] = float(user_parameters[i]) # In case float input is "1" or "0" as ints
-        
-      else:
-        print(f"Invalid input, not of type '{current_type.__name__}'.")   
-
-  return user_parameters
+def int_input(user_msg: str, lower_limit: int, upper_limit: int) -> int:
+  """Input method for parameters of integer values."""
+  user = None
+  while not user.isdigit() and lower_limit <= int(user) <= upper_limit:  
+    user = _user_input(user_msg + f" (between {lower_limit} and {upper_limit}): ")
+    if not user.isdigit() or not lower_limit <= int(user) <= upper_limit:
+      print(f"Input must be an integer between {lower_limit} and {upper_limit}, try again.")
+  return int(user)
 
 
+def float_input(user_input: str) -> float:
+  """Input method for parameters of floating point values."""
+  user = 'spam'
+  while not user.replace('.','',1).isdigit() and 0 <= float(user) <= 1:
+    # Between 0 and 1 since this is only used for reproduction  
+    user = _user_input(user_msg)
+    if not user.replace('.','',1).isdigit() or not 0 <= float(user) <= 1:
+      print(f"Input must be a decimal number (float) between 0 and 1 (i.e. a percentage), try again.")
+  return float(user)
+
+
+def update_animal(animal: parameters.Population) -> None:
+  """Function used to update population parameters for the animals of the simulation."""
+  animal.initial_size = int_input('Please enter an initial size of the population', 0, params.world.area())
+  animal.max_age = int_input('Please enter the max age for the population', 0, sys.maxsize)
+  animal.max_energy = int_input('Please enter the maximum amount of energy that a member of the population can attain', 0, sys.maxsize)
+  animal.metabolism = int_input('Please enter the metabolism of the population', 1, sys.maxsize)
+  animal.reproduction_probability = float_input('Please enter the reproduction probability of the population as a percentage between 0 and 1')
+  animal.reproduction_min_age = int_input('Please enter the minimum age for reproduction for the population', 0, animal.max_age())
+  animal.reproduction_min_energy = int_input('Please enter the minimum required energy for reproduction', 0, animal.max_energy())
+
+
+def set_execution() -> None:
+  """Used to update the execution settings for the simulation."""
+  params.execution.max_steps = int_input('Please enter the maximum amount of simulation steps', 0, sys.maxsize)
+  params.execution.batch = bool_input('batch', 'visual', 'Please choose a simulation mode')
 
 state = None # Initalize variable
 
 while state != start_menu[-1]: #As long as not "quit"
   if state not in start_menu:
-    quick_parameter_data = [params.world.north_south_length,
+    print(quick_param_msg.format(params.world.north_south_length,
                             params.world.west_east_length,
                             params.rabbits.initial_size,
                             params.foxes.initial_size,
                             params.execution.max_steps,
-                            params.execution.mode()]
-    
-    print(quick_param_msg.format(*quick_parameter_data))
+                            params.execution.mode()))
   state = menu(start_menu)
 
   if state == start_menu[0]: #Display parameters
     print("ALL PARAMETERS:")
     print(params)
 
-  if state == start_menu[1]: #Quick 
+  elif state == start_menu[1]: #Quick 
     # Assigns new values to the simulation
-    current_params = [params.world.north_south_length,
-                      params.world.west_east_length,
-                      params.rabbits.initial_size,
-                      params.foxes.initial_size,
-                      params.execution.max_steps,
-                      params.execution.batch]
-
-    new_params = input_parameters(current_params, quick_vars)
-    
-    params.world.north_south_length, params.world.west_east_length = new_params[:2]
-    params.rabbits.initial_size, params.foxes.initial_size = new_params[2:4]
-    params.execution.max_steps, params.execution.batch = new_params[4:]
+    params.world.north_south_length = int_input('Please enter a north/south length for the world', 1, sys.maxsize)
+    params.world.west_east_length = int_input('Please enter a west/east length for the world', 1, sys.maxsize)
+    params.rabbits.initial_size = int_input('Please enter an initial size of the population', 0, params.world.area())
+    params.foxes.initial_size = int_input('Please enter an initial size of the population', 0, params.world.area())
+    set_execution()
 
     print()
     state = None # This is only used to re-display the quick start parameters
     
-  if state == start_menu[2]: #Advanced setup
+  elif state == start_menu[2]: #Advanced setup
     while state not in advanced_menu[-1]: #As long as not "done" or "go back"
 
       state = menu(advanced_menu)
       if state == advanced_menu[0]: #World
-        current_params = [params.world.is_toroid,
-                          params.world.north_south_length,
-                          params.world.west_east_length]
-        new_params = input_parameters(current_params, adv_world)
-        params.world.is_toroid, params.world.north_south_length, params.world.west_east_length = new_params
+        params.world.is_toroid = bool_input('loop','walls', 'Should the world loop or have walls?')
+        params.world.north_south_length = int_input('Please enter a north/south length for the world', 1, sys.maxsize)
+        params.world.west_east_length = int_input('Please enter a west/east length for the world', 1, sys.maxsize)
         
-      if state == advanced_menu[1]: #Rabbit population
-        current_params = [params.rabbits.initial_size,
-                          params.rabbits.max_age,
-                          params.rabbits.max_energy,
-                          params.rabbits.metabolism,
-                          params.rabbits.reproduction_probability,
-                          params.rabbits.reproduction_min_age,
-                          params.rabbits.reproduction_min_energy]
-        new_params = input_parameters(current_params, adv_pop)
-        
-        params.rabbits.initial_size, params.rabbits.max_age, params.rabbits.max_energy, params.rabbits.metabolism = new_params[:4]
-        params.rabbits.reproduction_probability, params.rabbits.reproduction_min_age, params.rabbits.reproduction_min_energy = new_params[4:]
+      elif state == advanced_menu[1]: #Rabbit population
+        update_animal(params.rabbits)
+              
+      elif state == advanced_menu[2]: #Fox population
+        update_animal(params.foxes)
       
-      if state == advanced_menu[2]: #Fox population
-        current_params = [params.foxes.initial_size,
-                          params.foxes.max_age,
-                          params.foxes.max_energy,
-                          params.foxes.metabolism,
-                          params.foxes.reproduction_probability,
-                          params.foxes.reproduction_min_age,
-                          params.foxes.reproduction_min_energy]
-        new_params = input_parameters(current_params, adv_pop)
-        
-        params.foxes.initial_size, params.foxes.max_age, params.foxes.max_energy, params.foxes.metabolism = new_params[:4]
-        params.foxes.reproduction_probability, params.foxes.reproduction_min_age, params.foxes.reproduction_min_energy = new_params[4:]
-      
-      if state == advanced_menu[3]: #Execution
-        current_params = [params.execution.max_steps, params.execution.batch]
-        params.execution.max_steps, params.execution.batch = input_parameters(current_params, adv_exe)
+      elif state == advanced_menu[3]: #Execution
+        set_execution()
         
     state = None # This is only used to re-display the quick start parameters
 
-  if state == start_menu[3]: #Run
-    try:
-      sim_data = sim.run(params)
-      while state != reporting_menu[-1]: #As long as not "quit"
-        state = menu(reporting_menu)
+  elif state == start_menu[3]: #Run
+    sim_data = sim.run(params)
+    while state != reporting_menu[-1]: #As long as not "quit"
+      state = menu(reporting_menu)
         
-        if state == reporting_menu[0]: #Print summary
-          report.print_summary(sim_data)
-        if state == reporting_menu[1]: #Plot pop. size / time
-          report.plot_pop_size(sim_data)
-        if state == reporting_menu[2]: #Plot lifespan
-          report.plot_lifespan(sim_data)
-        if state == reporting_menu[3]: #Plot energy
-          report.plot_energy(sim_data)
-        if state == reporting_menu[4]: #Plot kills distribution
-          report.plot_kills(sim_data)
-
-    except AssertionError:
-      input('Invalid parameters detected, please redo setup (press enter).')
-      state = None
+      if state == reporting_menu[0]: #Print summary
+        report.print_summary(sim_data)
+      elif state == reporting_menu[1]: #Plot pop. size / time
+        report.plot_pop_size(sim_data)
+      elif state == reporting_menu[2]: #Plot lifespan
+        report.plot_lifespan(sim_data)
+      elif state == reporting_menu[3]: #Plot energy
+        report.plot_energy(sim_data)
+      elif state == reporting_menu[4]: #Plot kills distribution
+        report.plot_kills(sim_data)
       
